519ff92da115d63e21a0ac652cc83def0586e00c
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-05-11 11:22:41 +0200

    initial commit

---
+++ src/components/RockPaperScissors.jsx
@@ -0,0 +1,109 @@
+// We need the state hook
+import React, { useState } from 'react'
+import { string } from 'prop-types'
+import Bulb from '@ladrillo/lightbulb'
+
+// These constants could live in another module
+const weapons = ['🗿', '📜', '✂️']
+
+// It's less error-prone to work with variables than hard-coded strings
+const [rock, paper, scissors] = weapons
+
+
+export default function RockPaperScissors({ player }) {
+  // 1- 👉👉👉 CREATE SLICES OF APPLICATION STATE (USING TOP-LEVEL COMPONENT STATE)
+
+  // We need several slices of state, to keep track of:
+  //   - the number of rounds played
+  const [rounds, setRounds] = useState(1)
+
+  // const stuff = useState(1) // stuff is array of 2 things: slice, and the slice "adjustor"
+  // const rounds = stuff[0]
+  // const setRounds = stuff [1]
+
+  //   - the number of rounds won
+  const [wins, setWins] = useState(0)
+  //   - the number of rounds lost
+  const [loses, setLoses] = useState(0)
+  //   - the MESSAGE displayed on the screen
+  const [message, setMessage] = useState('Fight!')
+
+  // 2- 👉👉👉 We DO NOT need a slice for rounds tied, as we can calculate that info using other slices
+  // 3- 👉👉👉 REPLACE DOWN IN THE JSX THE HARD CODED PIECES OF DATA WITH PIECES OF STATE
+  // 4- 👉👉👉 ATTACH IN THE JSX A CLICK HANDLER THAT UPDATES STATE ON USER INTERACTION
+
+
+  const play = event => {
+    // 5- 👉👉👉 IMPLEMENT THE 'play' HANDLER!
+
+    // ⛔⛔⛔ DO NOT MUTATE STATE, EVER! PROVIDE A NEW ONE ⛔⛔⛔
+    // ⛔⛔⛔ DO NOT MUTATE STATE, EVER! PROVIDE A NEW ONE ⛔⛔⛔
+    // ⛔⛔⛔ DO NOT MUTATE STATE, EVER! PROVIDE A NEW ONE ⛔⛔⛔
+
+    // 5A- Update the round count
+    // rounds++
+
+    setRounds(rounds + 1) // <- a FRESH NEW STATE
+
+    // 5B- Pull out which weapon the user selected from the event.target
+    // this is the player weapon
+    const player = event.target.name
+    console.log(`YOU CLICKED ${player}`)
+
+    // 5C- Generate a random number (0, 1, 2) to select a weapon for the computer
+    // this is the computer weapon
+    const computer = weapons[Math.floor(Math.random() * 3)]
+
+    if (
+      /* 5D- it's a LOSS */
+      (player === rock && computer === paper) ||
+      (player === paper && computer === scissors) ||
+      (player === scissors && computer === rock)
+    ) {
+      // - Update the losses
+      setLoses(loses + 1)
+      // - Update the message
+      setMessage(`You: ${player}, They: ${computer}. You Lose!`)
+    } else if (
+      /* 5E- it's a WIN */
+      (player === rock && computer === scissors) ||
+      (player === paper && computer === rock) ||
+      (player === scissors && computer === paper)
+    ) {
+      // - Update the wins
+      setWins(wins + 1)
+      // - Update the message
+      setMessage(`You: ${player}, They: ${computer}. You Win!`)
+    } /* 5F- it's a TIE */ else {
+      // - Update the message
+      setMessage(`You: ${player}, They: ${computer}. its a tie`)
+    }
+  }
+
+  return (
+    <div className='rock-paper-scissors'>
+      <div className='player'>Welcome, {player}</div>
+      <h2>Round: {rounds}</h2>
+
+      <Bulb on color={loses > wins ? 'red' : 'blue'} size={100} />
+
+      <h3>won: {wins} | lost: {loses} | tied: {rounds - 1 - (wins + loses)}</h3>
+      <h1 className='message'>{message}</h1>
+
+      <div className='buttons'>
+        <h3>Choose wisely:</h3>
+        <div>
+          <button onClick={play} name={rock}>{rock}</button>
+          <button onClick={play} name={paper}>{paper}</button>
+          <button onClick={play} name={scissors}>{scissors}</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+RockPaperScissors.propTypes = { // OPTIONAL (but nice)
+  player: string.isRequired,
+}
+
+// STRETCH- 👉👉👉 ADD A RESTART BUTTON, ANYTHING YOU CAN IMAGINE, BRING OTHER COMPONENTS...
---
+++ src/index.js
@@ -0,0 +1,82 @@
+// 👉 Importing React libs from node_modules folder (see package.json)
+import React from 'react'                 // to make React components
+import { render } from 'react-dom'        // to append a React tree of components to the DOM
+import { string, bool } from 'prop-types' // to perform validation of props (optional)
+
+// 👉 Importing a React component from another file
+import RockPaperScissors from './components/RockPaperScissors'
+
+// 👉 Importing a React component from node_modules
+import Bulb from '@ladrillo/lightbulb'
+
+
+/*
+💥💥💥 A- Mounting a React element to the DOM (only once per app) 💥💥💥
+💥💥💥 A- Mounting a React element to the DOM (only once per app) 💥💥💥
+💥💥💥 A- Mounting a React element to the DOM (only once per app) 💥💥💥
+*/
+const id = 'helloWorld'
+const className = 'hello-world'
+const content = 'Hello, World!!!!!!!'
+const altContent = true ? 'HELLO, WORLD' : 'you will not see me'
+const size = 250
+render(
+  // 👉 1st ARG - React element (or React component "invoked"):
+  // <div className={className} id={id}>{altContent + 5}</div>,
+  // <div>
+  //   <Bulb on={true} color='red' size='150' />
+  //   <Bulb on={false} color='blue' size={size} />
+  //   <Bulb on={true} color='pink' size={200} />
+  // </div>,
+  // <Greet name='Ian' emphasis hate />,
+  <RockPaperScissors player='Ian' />,
+  // 👉 2nd ARG - DOM element to append to:
+  document.querySelector('#root')
+)
+
+
+/*
+💥💥💥 B- Example of a React component `Greet` being DECLARED 💥💥💥
+💥💥💥 B- Example of a React component `Greet` being DECLARED 💥💥💥
+💥💥💥 B- Example of a React component `Greet` being DECLARED 💥💥💥
+
+  - Name is capitalized
+  - Takes an object ("props") as its only argument
+  - Must return SOMETHING (null, empty string, elements...)
+  - React provides some built-in components (h1, div, etc)
+  - Attribute names with dashes are camelCased (except data- & aria-)
+  - The `class` attribute is `className`, `for` attribute is `htmlFor`
+  - We can INTERPOLATE 🔥 expressions 🔥 using curly braces
+  - We can interpolate attribute values, and content
+  - We can validate the props
+*/
+function Greet(props) { // takes data => returns element
+  const { name, emphasis, hate } = props
+  // if (hate) {
+  //   return 'I HATE YOU ' + name
+  // }
+  return (
+    <div>Hello, {name}. I am {emphasis && 'SO'} {hate ? 'angry' : 'happy'} to see you.</div>
+  )
+}
+Greet.propTypes = { // validation (optional but recommended)
+  name: string,
+  emphasis: bool,
+  hate: bool,
+}
+
+
+/*
+💥💥💥 C- Examples of `Greet` component being INVOKED 💥💥💥
+💥💥💥 C- Examples of `Greet` component being INVOKED 💥💥💥
+💥💥💥 C- Examples of `Greet` component being INVOKED 💥💥💥
+
+  - Components are "invoked" so they return elements
+  - Instead of invoking with parens, we invoke with < />
+  - Instead of passing args with parens, we pass them with attribute-like syntax
+  - Don't forget all tags need to close correctly
+
+  <Greet name='Ian' emphasis />              // props are { name: 'Ian', emphasis: true }
+  <Greet name='Michael' emphasis={false} />  // props are { name: 'Michael', emphasis: false }
+  <Greet name='Sarah' />                     // props are { name: 'Sarah' }
+*/
