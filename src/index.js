0189ab4a089ed6e86f3d844689b4d0ed24472bc7
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-06-08 14:06:03 +0200

    comments, deps

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ import FamilyTree from './components/FamilyTree'
 
 
 /*
-💥💥💥 A- Mounting a React element to the DOM (often only once per app) 💥💥💥
-💥💥💥 A- Mounting a React element to the DOM (often only once per app) 💥💥💥
-💥💥💥 A- Mounting a React element to the DOM (often only once per app) 💥💥💥
+💥💥💥 TASK 1A- Mounting a React element to the DOM (often only once per app) 💥💥💥
+  👉 execute the 'render' named export from react-dom
+  👉 1st ARG - React element (or React component "invoked")
+  👉 2nd ARG - DOM element to append to
 */
 
 // EXERCISE: render the RockPaperScissors to div#rockPaperScissors
@@ -29,12 +30,11 @@ render(
 
 
 /*
-💥💥💥 B- Example of a React component `Greet` being DECLARED 💥💥💥
-💥💥💥 B- Example of a React component `Greet` being DECLARED 💥💥💥
-💥💥💥 B- Example of a React component `Greet` being DECLARED 💥💥💥
+💥💥💥 TASK 1B- Example of a React component `Greet` being DECLARED 💥💥💥
 
   - Name is capitalized
-  - Takes an object ("props") as its only argument
+  - Takes an object ("props") as its one argument
+  - We often destructure the individual props
   - Must return SOMETHING (null, empty string, elements...)
   - React provides some built-in components (h1, div, etc)
   - Attribute names with dashes are camelCased (except data- & aria-)
@@ -55,18 +55,16 @@ function Greet(props) { // takes data => returns element
 
 
 /*
-💥💥💥 C- Examples of `Greet` component being INVOKED 💥💥💥
-💥💥💥 C- Examples of `Greet` component being INVOKED 💥💥💥
-💥💥💥 C- Examples of `Greet` component being INVOKED 💥💥💥
+💥💥💥 TASK 1C- Examples of a `Greet` component being INVOKED 💥💥💥
 
   - Components are "invoked" so they return elements
   - Instead of invoking with parens, we invoke with < />
   - Instead of passing args with parens, we pass them with attribute-like syntax
   - Don't forget all tags need to close correctly
 
-  <Greet personToGreet='Ian' emphasis />              // props are { personToGreet: 'Ian', emphasis: true }
-  <Greet personToGreet='Michael' emphasis={false} />  // props are { personToGreet: 'Michael', emphasis: false }
-  <Greet personToGreet='Sarah' />                     // props are { personToGreet: 'Sarah' }
+  <Greet person='Ian' emphasis />              // props is object with { person: 'Ian', emphasis: true }
+  <Greet person='Michael' emphasis={false} />  // props is object with { person: 'Michael', emphasis: false }
+  <Greet person='Sarah' />                     // props is object with { person: 'Sarah' }
 */
 
 // EXERCISE: render the Greet to div#greet passing the correct props
